bb90130c1ec2bf43f1ff13b113bf47f571b9d3aa
Chuck Konkol <ckonkol@gmail.com>
2022-02-11 15:02:41 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -50,7 +50,7 @@ const uiData = {
     valentinesFirstName: 'Lori',
     valentinesLastName: 'Konkol',
     headerMessage: 'Will you bee my Valentine? 🌹',
-    resultHeaderYes: '🏓 Woot! Woot! You said yes 🏓! <br> 😍💚💜💚💕💜💚💜😘’,
+    resultHeaderYes: 'Woot! Woot! You said yes ! 🏓🏓<br> 😍💚💜💚💕💜💚💜😘’,
     resultHeaderNo: 'You said no 😢',
     yesImgLink: yes1,
     noImgLink: 'https://media.giphy.com/media/wIhfELB4LvDhe/giphy.gif',
